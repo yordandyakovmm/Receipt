f5d8eb9b0aca7e3c2bf805a5aed97ac80f88ba4b
Yordan Dyakov <yordan.dyakov@mentormate.com>
2017-03-27 19:52:32 +0300

    Simple Diseing

---
+++ Receipt/Controllers/Companies.cs
@@ -0,0 +1,22 @@
+﻿using Receipt.DataContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Receipt.Controllers
+{
+    public class CompaniesController : Controller
+    {
+        [Authorize]
+        public ActionResult Index()
+        {
+            ReceiptDataContext dc = new ReceiptDataContext();
+            List<Company> companies = dc.Companies.ToList();
+            return View(companies);
+        }
+                
+        
+    }
+}
